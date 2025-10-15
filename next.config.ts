f304b78824611500f1ec67ffacfa6738b3d1bcc1
Eduardo Salas Araujo <flans@MacBookPro.attwifi.manager>
2025-10-15 09:02:39 -0700

    build: include prisma/dev.db in serverless functions

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 export default {
   /* config options here */
+  experimental: {
+    outputFileTracingIncludes: {
+      // Include our local SQLite + schema in all functions
+      '/**': ['./prisma/dev.db', './prisma/schema.prisma'],
+    },
+  },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 } satisfies import("next").NextConfig;
